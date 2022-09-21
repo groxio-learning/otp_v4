4d81700974b838203c34ceed0ca7a26a3e69d6a4
Oscar Amado <oscar@verypossible.com>
2022-09-21 09:02:17 -0500

    Lets create mutiple childs

--- enforcer/lib/enforcer/game.ex
+++ enforcer/lib/enforcer/game.ex
@@ -2,12 +2,14 @@ defmodule Enforcer.Game do
   use GenServer
   alias Enforcer.Game.{Board, ScoreBoard}
 
-  def start_link(answer \\ nil) do
-    GenServer.start_link(__MODULE__, answer)
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, nil, name: name)
   end
 
   def guess(server, turn) do
-    GenServer.cast(server, {:guess, turn})
+    server
+    |> GenServer.call({:guess, turn})
+    |> IO.puts()
   end
 
   @impl GenServer
@@ -16,17 +18,17 @@ defmodule Enforcer.Game do
   end
 
   @impl GenServer
-  def handle_cast({:guess, turn}, state) do
+  def handle_call({:guess, turn}, _from, state) do
     new_state =
       state
       |> Board.guess(turn)
 
     status = Board.get_status(new_state)
-    ScoreBoard.show(new_state, status)
+    view = ScoreBoard.show(new_state, status)
 
     case status do
-      :playing -> {:noreply, new_state}
-      _ -> {:stop, :game_over, state}
+      :playing -> {:reply, view, new_state}
+      _ -> {:stop, view, state}
     end
   end
 end
